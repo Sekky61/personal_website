e0efb25e9c9743cb09b40a519666597ed7a29f23
Michal Majer <misa@majer.cz>
2022-12-19 20:28:52 +0100

    sanity v3

---
+++ admin/schemas/tag.js
@@ -0,0 +1,9 @@
+export default {
+    name: 'tag',
+    title: 'Tag',
+    type: 'object',
+    fields: [
+        { name: 'label', type: 'string', title: 'Label' },
+        { name: 'value', type: 'string', title: 'Value' },
+    ]
+};
