5414c5a08471752624b46a775c821254d2cf9bf0
Michal Majer <misa@majer.cz>
2022-12-21 16:06:03 +0100

    404 render error

--- web/pages/404.tsx
+++ web/pages/404.tsx
@@ -4,9 +4,7 @@ export default function FourOhFour() {
     return <>
         <h1 className='text-5xl font-bold'>404 - Page Not Found</h1>
         <Link href="/">
-            <a>
-                Go back home
-            </a>
+            Go back home
         </Link>
     </>
 }
