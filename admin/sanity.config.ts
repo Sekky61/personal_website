7673fdda044e1344ff9a5ae8311340ce3761ba9b
Michal Majer <misa@majer.cz>
2023-01-02 17:45:26 +0100

    ul rendering fix

--- admin/sanity.config.ts
+++ admin/sanity.config.ts
@@ -4,35 +4,31 @@ import schemas from './schemas/schema';
 import { codeInput } from "@sanity/code-input";
 import { visionTool } from '@sanity/vision';
 
+const common = {
+    projectId: "3q20z5w8",
+    plugins: [
+        deskTool(),
+        codeInput(),
+        visionTool()
+    ],
+    schema: {
+        types: schemas,
+    },
+};
+
 export default defineConfig([
     {
         name: "prod_config",
         title: "Production dataset",
-        projectId: "3q20z5w8",
         dataset: "production",
         basePath: "/prod",
-        plugins: [
-            deskTool(),
-            codeInput(),
-            visionTool()
-        ],
-        schema: {
-            types: schemas,
-        },
+        ...common,
     },
     {
         name: "dev_config",
         title: "Development dataset",
-        projectId: "3q20z5w8",
         dataset: "development",
         basePath: "/dev",
-        plugins: [
-            deskTool(),
-            codeInput(),
-            visionTool()
-        ],
-        schema: {
-            types: schemas,
-        },
+        ...common,
     }
 ]);
