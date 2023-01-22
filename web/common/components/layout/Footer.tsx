ea60d12a99fbc6a16f6e17d3547a15cbeb64c118
Michal Majer <misa@majer.cz>
2023-01-22 12:58:51 +0100

    public repo

--- web/common/components/layout/Footer.tsx
+++ web/common/components/layout/Footer.tsx
@@ -3,8 +3,14 @@ const Footer = () => {
 
     return (
         <div className="border-t mt-10 py-4 px-4">
-            <div className="small-container">
-                Michal Majer, {year}
+            <div className="small-container flex-col">
+                <div>Michal Majer, {year}</div>
+                <div>
+                    <span>
+                        Got a problem or feedback? Check out&nbsp;
+                    </span>
+                    <a target="_blank" href="https://github.com/Sekky61/personal_website/issues" rel="noreferrer noopener" className="link">issues on Github</a>
+                </div>
             </div>
         </div>
     )
