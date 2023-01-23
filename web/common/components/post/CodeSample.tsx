5cadadb1d112cb3f5375020af465bbd2a7eeaa55
Michal Majer <misa@majer.cz>
2023-01-23 20:47:08 +0100

    Code rendering refactor

--- web/common/components/post/CodeSample.tsx
+++ web/common/components/post/CodeSample.tsx
@@ -3,16 +3,9 @@ import { createElement, Prism as SyntaxHighlighter } from 'react-syntax-highligh
 import { a11yDark } from 'react-syntax-highlighter/dist/cjs/styles/prism'; // css object
 
 import { CopyIcon, CheckmarkIcon } from '@common/svg/CopyIcon';
+import { getRenderer, Token } from '@common/utils/codeRendering';
 
-type TokenType = 'error' | 'warning' | 'info';
-
-type Token = {
-    type: TokenType;
-    line: number;
-    message: string;
-}
-
-type CodeSampleProps = {
+interface CodeSampleProps {
     value: {
         fileName: string;
         lineStart?: number;
@@ -26,86 +19,17 @@ type CodeSampleProps = {
     }
 }
 
-const lineStyle = {
-    highlighted: {
-        line: ['bg-lime-700/30', 'rounded'],
-        text: ['token-message', `highlight-token`]
-    },
-    error: {
-        line: ['bg-red-700/25', 'rounded'],
-        text: ['token-message', `error-token`]
-    },
-    warning: {
-        line: ['bg-yellow-700/25', 'rounded'],
-        text: ['token-message', `warning-token`]
-    },
-    info: {
-        line: ['bg-blue-700/25', 'rounded'],
-        text: ['token-message', `info-token`]
-    },
-};
-
-function rowRenderer({ row, stylesheet, useInlineStyles, rowNumber, tokens, highlightedLines }: any) {
-    // find if there is a token for this line
-    const token = tokens.find((t: any) => t.line === rowNumber);
-    if (token) {
-        const tokenType = token.type as TokenType;
-        const style = lineStyle[tokenType];
-        row.properties.className = style.line;
-        row.children.push({
-            type: 'element',
-            tagName: 'span',
-            properties: {
-                className: style.text
-            },
-            children: [
-                {
-                    type: 'text',
-                    value: token.message
-                }
-            ]
-        });
-    } else if (highlightedLines.includes(rowNumber)) {
-        row.properties.className = lineStyle.highlighted.line;
-    } else {
-        // It needs to be reset
-        row.properties.className = [];
-    }
-
-    let rowElement = createElement({
-        node: row,
-        stylesheet,
-        useInlineStyles,
-        key: rowNumber,
-    });
-
-    return rowElement;
-}
-
-function createRendererWithContext(tokens: Token[], highlightedLines: number[]) {
-    return ({ rows, stylesheet, useInlineStyles }: any) => {
-        const renderedLines = rows.map((row: any, number: number) => {
-            const lineNumber = number + 1;
-            return rowRenderer({ row, stylesheet, useInlineStyles, rowNumber: lineNumber, tokens, highlightedLines })
-        })
-
-        return <div>{renderedLines}</div>
-    }
-}
-
 // Options here: https://github.com/react-syntax-highlighter/react-syntax-highlighter
 const CodeSample = ({ value }: CodeSampleProps) => {
     const { fileName, lineStart, tokens, code: { code, language, highlightedLines = [] }, output } = value;
     const hasFileName = fileName !== undefined && fileName !== null && fileName !== '';
+    const hasOutput = output !== undefined && output !== null && output !== '';
     const startingLineNumber = lineStart !== undefined && lineStart !== null ? lineStart : 1;
+    const renderer = getRenderer(tokens, highlightedLines);
 
     // Patch the style - remove margin
     a11yDark['pre[class*="language-"]'].margin = '0px';
     a11yDark['pre[class*="language-"]'].paddingTop = '40px';
-
-    const renderer = createRendererWithContext(tokens, highlightedLines);
-
-    const hasOutput = output !== undefined && output !== null && output !== '';
 
     return (
         <div>
