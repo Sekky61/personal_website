a1f8c9cd807a8a94943a9cd755d51e59dc24386d
Sekky61 <misa@majer.cz>
2024-12-19 21:03:04 +0100

    css passes checks

---
+++ web/scripts/generateTailwind.ts
@@ -0,0 +1,21 @@
+import { loadTheme, MaterialTheme } from "./generateTheme";
+
+function convertToTailwindColors(theme: MaterialTheme) {
+  console.log("@theme {\n");
+  for(const paletteName in theme.palettes) {
+    const palette = theme.palettes[paletteName];
+    for(const colorName in palette) {
+      //   --color-primary-40: var(--md-ref-palette-primary40);
+      const line = `  --color-${paletteName}-${colorName}: var(--md-ref-palette-${paletteName}${colorName});`;
+      console.log(line);
+    }
+  }
+  console.log("\n");
+}
+
+async function main() {
+  const theme = await loadTheme("./material-theme.json");
+  convertToTailwindColors(theme);
+}
+
+main();
