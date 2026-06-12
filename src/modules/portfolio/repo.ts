f48c16192969117a35564de8915f4988b457322a
Sekky61 <misa@majer.cz>
2026-06-12 16:35:41 +0200

    feat: repo schema

--- src/modules/portfolio/repo.ts
+++ src/modules/portfolio/repo.ts
@@ -1,13 +1,21 @@
-export type Repo = {
-  name: string;
-  link: string;
-  description: string;
-  technologies: string[];
-  img: string | null;
-  githubData: {
-    name: string;
-    description: string | null;
-    updated_at: string;
-    language: string;
-  };
-};
+import * as z from "zod";
+
+const RepoGithubDataSchema = z.object({
+  name: z.string().nonempty(),
+  link: z.string().nonempty(),
+  updated_at: z.string().nonempty(),
+  language: z.string().nonempty(),
+});
+
+export const RepoSchema = z.object({
+  name: z.string().nonempty(),
+  link: z.string().nonempty(),
+  description: z.string().nonempty().max(200, {
+    error: "Description should be short. It is displayed in a card.",
+  }),
+  technologies: z.array(z.string().nonempty()),
+  img: z.union([z.string(), z.null()]),
+  githubData: RepoGithubDataSchema,
+});
+
+export type Repo = z.infer<typeof RepoSchema>;
